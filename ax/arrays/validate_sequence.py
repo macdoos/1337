b6ce6dbd29a8fcf22a49f56ca8217f036b4dc8c5
macdoos <bigmacdoos@gmail.com>
2024-06-11 00:13:05 +0200

    Validate Sequence

---
+++ ax/arrays/validate_sequence.py
@@ -0,0 +1,16 @@
+# O(N) space | O(1) time
+def isValidSubsequence(array, sequence):
+    arrIdx = 0
+    seqIdx = 0
+    N = len(array)
+
+    while arrIdx < N and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+
+    return seqIdx == len(sequence)
+
+input = [5, 1, 22, 25, 6, -1, 8, 10]
+seq = [1, 6, -1, 10]
+print(isValidSubsequence(input, seq))
