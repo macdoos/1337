b4ca300bd9ccc86710b40bbc6993a6838117cf4c
macdoos <bigmacdoos@gmail.com>
2024-08-31 04:00:27 +0200

    Merge sorted array

---
+++ dsa/leetcode/merge_sorted_array.py
@@ -0,0 +1,22 @@
+def merge(nums1, m, nums2, n):
+  x, y = m - 1, n - 1
+
+  for z in range(m + n - 1, -1, -1):
+      if x < 0:
+          nums1[z] = nums2[y]
+          y -= 1
+      elif y < 0:
+          break
+      elif nums1[x] > nums2[y]:
+          nums1[z] = nums1[x]
+          x -= 1
+      else:
+          nums1[z] = nums2[y]
+          y -= 1 
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+merge(nums1, m, nums2, n)
+print(nums1)
