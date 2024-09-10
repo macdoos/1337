9e2807894327e3267f837190965bda26776b79fe
macdoos <bigmacdoos@gmail.com>
2024-09-10 17:26:55 +0200

    Find String

---
+++ python/find_string.py
@@ -0,0 +1,12 @@
+def count_substring(string, sub_string):
+    count = 0
+    
+    for i in range(len(string) - len(sub_string) + 1):
+        if string[i:i+len(sub_string)] == sub_string:
+            count += 1
+    
+    return count
+
+string = 'ABCDCDC'
+sub_string = 'CDC'
+print(count_substring(string, sub_string))
